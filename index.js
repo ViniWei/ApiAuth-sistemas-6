44f33f609b4b93719354e21b87f62b6805150731
ViniWei <viniciusweigert152@gmail.com>
2025-04-27 21:01:28 -0300

    Added base of verifyToken route

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const PORT = 6767;
 const app = express();
+const secretKey = "A@#PJ!#)I(Dj323243";
 
 app.use(express.json());
+
 
 const data = [];
 
@@ -23,7 +26,6 @@ app.post("/registerUser", async(req, res) => {
     };
 
     data.push(user);
-    console.log(data);
 
     res.send("success.");
 });
@@ -43,7 +45,17 @@ app.post("/login", async(req, res) => {
         res.status(400).send("Not authorized.");
     }
 
-    res.status(200).send("User logged in.");
+    const token = jwt.sign({ email: user.email }, secretKey);
+
+    res.status(200).send(token);
+})
+
+app.get("/verifyToken", (req, res) => {
+    const token = req.header("Authorization");
+
+    if (!token) {
+        return res.status(401).json({ error: 'Access denied' })
+    }
 })
 
 app.listen(PORT, () => { console.log("Listening on port:", PORT) });
